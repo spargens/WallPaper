b56e682f7473ad0258a8d3d5ddbb224f67f9617a
spargens <amartyasingh1010@gmail.com>
2024-06-07 12:27:14 +0530

    addressing issue #5

--- imgs.js
+++ imgs.js
@@ -1,4 +1,6 @@
 export default imgs = [
+  'https://images.pexels.com/photos/7515368/pexels-photo-7515368.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
+  'https://images.pexels.com/photos/25194072/pexels-photo-25194072/free-photo-of-pile-of-leaflets-on-a-chair.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load',
   'https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=800',
   'https://images.pexels.com/photos/1198817/pexels-photo-1198817.jpeg?auto=compress&cs=tinysrgb&w',
   'https://images.pexels.com/photos/1227497/pexels-photo-1227497.jpeg?auto=compress&cs=tinysrgb&w=800',
